c91c924786f7299e3648dbf36d071bd015d19cd5
Harshvardhan Karn <harshvardhan@deepfence.io>
2024-07-17 11:57:52 +0000

    (feat) add tar extractor

---
+++ integ-tests/tar.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"os/exec"
+
+	"github.com/deepfence/match-scanner/pkg/config"
+	"github.com/deepfence/match-scanner/pkg/extractor"
+	"github.com/deepfence/match-scanner/pkg/scanner"
+)
+
+func main() {
+	imgTar := "/tmp/xmrig/image.tar"
+	imageName := "metal3d/xmrig:latest"
+	cmd := exec.Command("skopeo", []string{"copy", "--insecure-policy", "--src-tls-verify=false",
+		"docker://" + imageName, "docker-archive:" + imgTar}...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	errorOnRun := cmd.Run()
+	if errorOnRun != nil {
+		println("Error: ", errorOnRun.Error())
+		println("stderr: ", stderr.String())
+		return
+	}
+
+	cfg, err := config.ParseConfig("integ-tests/config.yaml")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	extract, err := extractor.NewTarExtractor(config.Config2Filter(cfg), "", imgTar)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer extract.Close()
+
+	scanner.ApplyScan(context.Background(), extract, func(f extractor.ExtractedFile) {
+		println(f.Filename)
+	})
+}
---
+++ pkg/extractor/tar.go
@@ -0,0 +1,86 @@
+package extractor
+
+import (
+	"archive/tar"
+	"context"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+
+	"github.com/deepfence/match-scanner/pkg/config"
+)
+
+type TarExtractor struct {
+	tarReader      *tar.Reader
+	layerTarReader fs.FS
+	lastLayerErr   error
+	tarPath        string
+	filters        config.Filters
+	ctx            context.Context
+	cancel         context.CancelFunc
+	files          chan fileErr
+}
+
+func NewTarExtractor(filters config.Filters, imageNamespace, tarPath string) (*TarExtractor, error) {
+	f, err := os.Open(tarPath)
+	if err != nil {
+		return nil, err
+	}
+
+	tr := tar.NewReader(f)
+
+	return &TarExtractor{
+		tarReader: tr,
+		filters:   filters,
+		tarPath:   tarPath,
+	}, nil
+
+}
+
+func (te *TarExtractor) nextLayerFile() (ExtractedFile, error) {
+	fErr, opened := <-te.files
+
+	if !opened {
+		return ExtractedFile{}, io.EOF
+	}
+
+	if fErr.err != nil {
+		return ExtractedFile{}, fErr.err
+	}
+
+	return ExtractedFile{
+		Filename:    fErr.fpath,
+		Content:     fErr.f.(io.ReadSeeker),
+		ContentSize: int(fErr.fsize),
+		Cleanup: func() {
+			fErr.f.Close()
+		},
+	}, nil
+}
+
+func (te *TarExtractor) NextFile() (ExtractedFile, error) {
+	for {
+		if te.layerTarReader != nil {
+			file, err := te.nextLayerFile()
+			if err != nil {
+				te.layerTarReader = nil
+				goto next_layer
+			}
+			return file, nil
+		}
+	next_layer:
+		_, err := te.tarReader.Next()
+		if err != nil {
+			return ExtractedFile{}, err
+		}
+		te.layerTarReader, te.ctx, te.cancel, te.files, err = WalkLayer(te.tarReader, te.filters)
+		if err != nil {
+			fmt.Printf("WalkLayer error: %v\n", err)
+		}
+	}
+}
+
+func (te *TarExtractor) Close() error {
+	return os.Remove(te.tarPath)
+}
